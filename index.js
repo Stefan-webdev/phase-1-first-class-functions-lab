e66a7e9c16df81ddb88fce076f160fa1e253e753
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-07-15 15:23:07 +0930

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = 
+function(drivers) {
+    return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = 
+    function(drivers) {
+        return drivers.slice(2, 4)
+    }
+
+    const  selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+    const createFareMultiplier = function(multiplier) {
+        return function(fare) {
+            return fare * multiplier
+        }
+    }
+    //const createFareMultiplier = (a, b) => a * b
+
+    const fareDoubler = (a, b) => a * 2;
+
+    const fareTripler = (a, b) => a * 3;
+
+    const selectDifferentDrivers = function(drivers, selectingFunction) {
+        return selectingFunction(drivers);
+      };
